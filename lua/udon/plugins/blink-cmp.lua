8c68d1deef10bf224ac6028d615548acb8993490
udonc <28778487+udonc@users.noreply.github.com>
2026-01-05 07:43:06 +0900

    update

--- lua/udon/plugins/blink-cmp.lua
+++ lua/udon/plugins/blink-cmp.lua
@@ -2,6 +2,9 @@ return {
 	"saghen/blink.cmp",
 	version = "1.*",
 	opts = {
+		keymap = {
+			preset = "enter"
+		},
 		sources = {
 			-- add lazydev to your completion providers
 			default = { "lazydev", "lsp", "path", "snippets", "buffer" },
--- lua/udon/plugins/lualine.lua
+++ lua/udon/plugins/lualine.lua
@@ -1,6 +1,42 @@
 return {
 	'nvim-lualine/lualine.nvim',
 	dependencies = { 'nvim-tree/nvim-web-devicons' },
+	opts = function(_, opts)
+		opts.sections = opts.sections or {}
+		opts.sections.lualine_c = opts.sections.lualine_c or {}
+		opts.sections.lualine_x = opts.sections.lualine_x or {}
+
+		-- Copilot status
+		table.insert(opts.sections.lualine_c, {
+			function()
+				return " "
+			end,
+			color = function()
+				local status = require("sidekick.status").get()
+				if status then
+					return status.kind == "Error" and "DiagnosticError" or status.busy and "DiagnosticWarn" or "Special"
+				end
+			end,
+			cond = function()
+				local status = require("sidekick.status")
+				return status.get() ~= nil
+			end,
+		})
+
+		-- CLI session status
+		table.insert(opts.sections.lualine_x, 2, {
+			function()
+				local status = require("sidekick.status").cli()
+				return " " .. (#status > 1 and #status or "")
+			end,
+			cond = function()
+				return #require("sidekick.status").cli() > 0
+			end,
+			color = function()
+				return "Special"
+			end,
+		})
+	end,
 	config = function(opts)
 		require("lualine").setup(opts)
 	end,
