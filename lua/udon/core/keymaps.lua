ad156878b44e6eb409f10600c5d23d7f63843513
udonc <28778487+udonc@users.noreply.github.com>
2026-01-05 02:06:08 +0900

    update

--- lua/udon/core/keymaps.lua
+++ lua/udon/core/keymaps.lua
@@ -1,13 +1,11 @@
 vim.g.mapleader = " "
 
 -- editor split settings
-vim.keymap.set("n", "<Leader>ss", ":vsplit<CR>")
-vim.keymap.set("n", "<Leader>sc", ":close<CR>")
-vim.keymap.set("n", "<Leader>so", ":only<CR>")
+vim.keymap.set("n", "<C-s>", ":vsplit<CR>")
+vim.keymap.set("n", "<C-c>", ":close<CR>")
+vim.keymap.set("n", "<C-o>", ":only<CR>")
 
-vim.keymap.set("n", "<C-h>", "<C-w>h")
-vim.keymap.set("n", "<C-j>", "<C-w>j")
-vim.keymap.set("n", "<C-k>", "<C-w>k")
-vim.keymap.set("n", "<C-l>", "<C-w>l")
+vim.keymap.set("n", "<C-j>", "<C-w>w")
+vim.keymap.set("n", "<C-k>", "<C-w>W")
 
 vim.keymap.set("n", "<Esc><Esc>", "<Cmd>nohlsearch<CR>")
