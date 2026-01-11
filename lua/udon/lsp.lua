435efebab5b0bac69ef9cc579ee4958f71a9ef3e
udonc <28778487+udonc@users.noreply.github.com>
2026-01-12 04:27:02 +0900

    update

--- lua/udon/lsp.lua
+++ lua/udon/lsp.lua
@@ -2,6 +2,10 @@ vim.lsp.enable({
 	-- nvim-lspconfig で"lua_ls"という名前で設定したプリセットが読まれる
 	-- https://github.com/neovim/nvim-lspconfig/blob/master/lsp/lua_ls.lua
 	"lua_ls",
-	-- 他の言語サーバーの設定
-	-- "gopls",
+	-- TypeScript/JavaScript
+	"ts_ls",
+	-- Markdown
+	"marksman",
+	-- JSON
+	"jsonls",
 })
--- lua/udon/plugins/mini-surround.lua
+++ lua/udon/plugins/mini-surround.lua
@@ -1,1 +1,7 @@
-return { 'nvim-mini/mini.surround', version = '*' }
+return {
+	'nvim-mini/mini.surround',
+	version = '*',
+	config = function()
+		require("mini.surround").setup()
+	end
+}
