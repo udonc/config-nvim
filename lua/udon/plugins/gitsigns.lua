26541712130119318805e5e8f0eb982317f27e3b
udonc <28778487+udonc@users.noreply.github.com>
2026-01-17 04:33:37 +0900

    update

--- lua/udon/plugins/gitsigns.lua
+++ lua/udon/plugins/gitsigns.lua
@@ -4,4 +4,48 @@ return {
 	opts = {
 		current_line_blame = true,
 	},
+	keys = {
+		{
+			"]c",
+			function()
+				require("gitsigns").nav_hunk("next")
+			end,
+			desc = "Next Hunk",
+		},
+		{
+			"[c",
+			function()
+				require("gitsigns").nav_hunk("prev")
+			end,
+			desc = "Previous Hunk",
+		},
+		{
+			"<leader>hs",
+			function()
+				require("gitsigns").stage_hunk()
+			end,
+			desc = "Stage Hunk",
+		},
+		{
+			"<leader>hr",
+			function()
+				require("gitsigns").reset_hunk()
+			end,
+			desc = "Reset Hunk",
+		},
+		{
+			"<leader>hp",
+			function()
+				require("gitsigns").preview_hunk_inline()
+			end,
+			desc = "Preview Hunk Inline",
+		},
+		{
+			"<leader>hb",
+			function()
+				require("gitsigns").blame_line()
+			end,
+			desc = "Blame Line",
+		},
+	},
 }
