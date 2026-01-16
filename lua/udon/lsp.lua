5659e86adf2535fdd1c4a493c6c7a066db51d250
udonc <28778487+udonc@users.noreply.github.com>
2026-01-17 01:51:17 +0900

    add diagnostic config

--- lua/udon/lsp.lua
+++ lua/udon/lsp.lua
@@ -1,3 +1,12 @@
+vim.diagnostic.config({
+	virtual_text = false,
+	virtual_lines = true,
+	signs = true,
+	underline = true,
+	update_in_insert = false,
+	severity_sort = true,
+})
+
 vim.lsp.enable({
 	-- nvim-lspconfig で"lua_ls"という名前で設定したプリセットが読まれる
 	-- https://github.com/neovim/nvim-lspconfig/blob/master/lsp/lua_ls.lua
