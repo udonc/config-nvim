0b74208fc9be7fa88e17b9cf0f1f0b2ea7f69c31
udonc <28778487+udonc@users.noreply.github.com>
2026-01-19 03:23:11 +0900

    update

--- lua/udon/core/keymaps.lua
+++ lua/udon/core/keymaps.lua
@@ -9,6 +9,3 @@ vim.keymap.set("n", "<C-j>", "<C-w>w")
 vim.keymap.set("n", "<C-k>", "<C-w>W")
 
 vim.keymap.set("n", "<Esc><Esc>", "<Cmd>nohlsearch<CR>")
-
-vim.keymap.set("t", "<C-u>", "<C-\\><C-N>k", {}) -- Scroll up using Ctrl + U
-vim.keymap.set("t", "<C-d>", "<C-\\><C-N>j", {}) -- Scroll down using Ctrl + D
