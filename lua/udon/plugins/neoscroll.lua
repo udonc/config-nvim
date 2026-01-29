2f2dd7d6ad94723997882405c214e8aec85f49fe
udonc <28778487+udonc@users.noreply.github.com>
2026-01-29 14:13:04 +0900

    update

--- lua/udon/plugins/neoscroll.lua
+++ lua/udon/plugins/neoscroll.lua
@@ -2,7 +2,25 @@ return {
 	"karb94/neoscroll.nvim",
 	event = "VeryLazy",
 	opts = {
-		mappings = { "<C-u>", "<C-d>", "<C-b>", "<C-f>", "zt", "zz", "zb" },
+		mappings = { "<C-b>", "<C-f>", "zt", "zz", "zb" },
 		easing = "quadratic",
 	},
+	config = function(_, opts)
+		local neoscroll = require("neoscroll")
+		neoscroll.setup(opts)
+
+		local duration = 250
+
+		local function scroll_and_center(scroll_func)
+			return function()
+				scroll_func({ duration = duration })
+				vim.defer_fn(function()
+					neoscroll.zz({ half_win_duration = 100 })
+				end, duration + 10)
+			end
+		end
+
+		vim.keymap.set({ "n", "v", "x" }, "<C-u>", scroll_and_center(neoscroll.ctrl_u), { silent = true })
+		vim.keymap.set({ "n", "v", "x" }, "<C-d>", scroll_and_center(neoscroll.ctrl_d), { silent = true })
+	end,
 }
