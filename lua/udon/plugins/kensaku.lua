8f7906d30ce3fd849cf066c02028db4014bc6b7b
udonc <28778487+udonc@users.noreply.github.com>
2026-02-04 15:56:12 +0900

    update

--- lua/udon/plugins/kensaku.lua
+++ lua/udon/plugins/kensaku.lua
@@ -1,11 +1,5 @@
 return {
-	{
-		"vim-denops/denops.vim",
-		lazy = false,
-	},
-	{
-		"lambdalisue/kensaku.vim",
-		dependencies = { "vim-denops/denops.vim" },
-		lazy = false,
-	},
+	"lambdalisue/kensaku.vim",
+	dependencies = { "vim-denops/denops.vim" },
+	lazy = false,
 }
